8a19cc6fbfd90d19088a08ca7e72772161aa9d86
vivmagarwal <vivmagarwal@gmail.com>
2024-01-01 21:55:13 +0530

    fix: conda and node in bash and zsh.

--- setup.sh
+++ setup.sh
@@ -19,6 +19,16 @@ fi
 # Execute Makefile for setup
 make setup
 
+# Initialize fnm and conda for Bash
+echo 'eval "$(fnm env)"' >> ~/.bashrc
+echo 'source ~/miniconda3/etc/profile.d/conda.sh' >> ~/.bashrc
+echo 'eval "$(fnm env)"' >> ~/.bash_profile
+echo 'source ~/miniconda3/etc/profile.d/conda.sh' >> ~/.bash_profile
+
+# Initialize fnm and conda for Zsh
+echo 'eval "$(fnm env)"' >> ~/.zshrc
+echo 'source ~/miniconda3/etc/profile.d/conda.sh' >> ~/.zshrc
+
 echo "Please remember to enable Settings Sync in Visual Studio Code manually."
 echo "Use 'Code -> Preferences -> Turn on Preferences Sync...' to sync your settings, extensions, and more."
 echo "Setup completed."
